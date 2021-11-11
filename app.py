af7b3a955386fe22fb31310258a4f8b3b19eab4f
Stanley <stanley.jeong@shopify.com>
2021-11-10 18:05:02 -0800

    resourceful routes completed for donations resource, new templates and partials for view

--- app.py
+++ app.py
@@ -1,16 +1,20 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 import os
-# from bson.objectid import ObjectId
+from pymongo import MongoClient
+from bson.objectid import ObjectId
 
+# MONGO_URI is Config Var for heroku later.
+host = os.environ.get('MONGO_URI', 'mongodb://localhost:27017/charity_tracker')
+client = MongoClient(host=host)
+db = client.get_default_database()
+
+# Donations resource in our MongoDB
+donations = db.donations
 
 
 app = Flask(__name__)
 
 
-# ROOT Route
-@app.route('/')
-def charity_tracker_index():
-  return render_template('new_donation.html') 
 
 
 # GET login page
@@ -26,7 +30,7 @@ def login():
     'password': request.form.get('password')
   }
   print(user)
-  return render_template('new_donation.html')
+  return render_template('donations_new.html')
 
 # GET sign up page
 @app.route('/signup')
@@ -44,22 +48,76 @@ def signup():
   print(new_user)
   return render_template('login.html')
 
-# GET the form to track donation
-@app.route('/donation/new')
-def track_donation():
-  return render_template('new_donation.html') 
 
-# POST - SUBMIT a donation
-@app.route('/donation', methods=['POST'])
+
+
+
+
+""" DONATIONS - RESTFUL ROUTES """
+
+# GET - INDEX ROOT Route - HOME PAGE, ALL DONATIONS
+@app.route('/')
+def donations_index():
+  return render_template('donations_index.html', donations=donations.find())
+
+# GET - SHOW a specific donation from donation_id
+@app.route('/donations/<donation_id>')
+def donation_show_one(donation_id):
+  donation = donations.find_one({'_id': ObjectId(donation_id)})
+  return render_template('donation_show_one.html', donation=donation)
+
+# GET - form to add NEW donation
+@app.route('/donations/new')
+def track_donation():
+  donation = {}
+  return render_template('donations_new.html', title='New Donation', donation=donation) 
+
+# POST - CREATE / SUBMIT a donation
+@app.route('/donations', methods=['POST'])
 def donation_submit():
   donation = {
     'charity_name': request.form.get('charity_name'),
-    'amount_in_cents': request.form.get('donation_amount'),
-    'date': request.form.get('date_donated'),
+    'donation_amount': request.form.get('donation_amount'),
+    'date_donated': request.form.get('date_donated'),
   }
-  print(donation)
-  # insert_one(donation) to the donations database
-  return render_template('new_donation.html')
+  # WRITES TO THE DB
+  donations.insert_one(donation)
+  return redirect(url_for('donations_index'))
+
+# GET - EDIT form
+@app.route('/donations/<donation_id>/edit')
+def donation_edit_page(donation_id):
+  donation = donations.find_one({'_id': ObjectId(donation_id)})
+  return render_template('donations_edit.html', donation=donation, title='Edit Donation')
+
+# PUT/PATCH - UPDATE a donation
+@app.route('/donations/<donation_id>', methods=['POST'])
+def donation_update(donation_id):
+  # The newly updated form data
+  updated_donation = {
+    'charity_name': request.form.get('charity_name'),
+    'donation_amount': request.form.get('donation_amount'),
+    'date_donated': request.form.get('date_donated'),
+  }
+  # Set that former donation from db to this updated one
+  donations.update_one(
+    {'_id': ObjectId(donation_id)},
+    {'$set': updated_donation}
+  )
+  return redirect(url_for('donations_index'))
+
+# DELETE - a donation 
+# USING AN <a> TAG WITHOUT POST METHOD WORKS
+@app.route('/donations/<donation_id>/delete')
+def donations_delete(donation_id):
+  print(donation_id)
+  donations.delete_one({'_id': ObjectId(donation_id)})
+  return redirect(url_for('donations_index'))
+
+
+
+
+
 
 
 # GET Profile page
@@ -71,6 +129,7 @@ def donor_profile():
 @app.route('/charity')
 def charity_profile():
   return render_template('charity.html')
+
 
 
 
