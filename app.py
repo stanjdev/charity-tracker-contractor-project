3f91d6e5cc71dc31eccc4962a9ddd5f9a456d8cc
Stanley <stanley.jeong@shopify.com>
2021-11-17 16:29:13 -0800

    delete charity, dropdown menu of existing charities when adding new donation, case sensitivity for charity names

--- app.py
+++ app.py
@@ -19,6 +19,8 @@ app = Flask(__name__)
 
 
 
+
+
 """ DONATIONS - RESTFUL ROUTES """
 
 # GET - INDEX ROOT Route - HOME PAGE, ALL DONATIONS
@@ -36,20 +38,71 @@ def donation_show_one(donation_id):
 @app.route('/donations/new')
 def track_donation():
   donation = {}
-  return render_template('donations_new.html', title='New Donation', donation=donation) 
+  return render_template('donations_new.html', title='New Donation', donation=donation, charities=charities.find()) 
 
 # POST - CREATE / SUBMIT a donation
 @app.route('/donations', methods=['POST'])
 def donation_submit():
   donation = {
-    'charity_name': request.form.get('charity_name'),
+    'charity_name': request.form.get('charity_name').title(),
     'donation_amount': request.form.get('donation_amount'),
     'date_donated': request.form.get('date_donated'),
   }
   # WRITES TO THE donations DB
   donations.insert_one(donation)
 
-  # Check if charity with same name exists. If not, insert one
+  # Create new charity if that name did not already exist
+  create_new_charity(donation)
+
+  return redirect(url_for('donations_index'))
+
+# GET - EDIT form
+@app.route('/donations/<donation_id>/edit')
+def donation_edit_page(donation_id):
+  donation = donations.find_one({'_id': ObjectId(donation_id)})
+  return render_template('donations_edit.html', donation=donation, title='Edit Donation', charities=charities.find())
+
+# PUT/PATCH - UPDATE a donation
+@app.route('/donations/<donation_id>', methods=['POST'])
+def donation_update(donation_id):
+  # The newly updated form data
+  updated_donation = {
+    'charity_name': request.form.get('charity_name').title(),
+    'donation_amount': request.form.get('donation_amount'),
+    'date_donated': request.form.get('date_donated'),
+  }
+  # Set that former donation from db to this updated one
+  donations.update_one(
+    {'_id': ObjectId(donation_id)},
+    {'$set': updated_donation}
+  )
+  # Create new charity if that name did not already exist
+  create_new_charity(updated_donation)
+
+  return redirect(url_for('donations_index'))
+
+# DELETE - a donation 
+# USING AN <a> TAG WITHOUT POST METHOD WORKS
+@app.route('/donations/<donation_id>/delete')
+def donations_delete(donation_id):
+  donations.delete_one({'_id': ObjectId(donation_id)})
+  return redirect(url_for('donations_index'))
+
+
+
+# Helper Function to create new charity
+def create_new_charity(donation):
+  """ 
+  ## DocString
+  ### "Check if charity with same name exists. If not, insert one."
+
+  ### Parameters
+  @donation: donation object that includes charity_name, donation_amount, and date_donated
+
+  ### Returns
+  None. inserts a new charity if it didn't exist
+   """
+
   existing_charity = charities.find_one({'name': donation['charity_name']})
   if not existing_charity:
     charity = {
@@ -59,40 +112,13 @@ def donation_submit():
     }
     charities.insert_one(charity)
 
-  return redirect(url_for('donations_index'))
-
-# GET - EDIT form
-@app.route('/donations/<donation_id>/edit')
-def donation_edit_page(donation_id):
-  donation = donations.find_one({'_id': ObjectId(donation_id)})
-  return render_template('donations_edit.html', donation=donation, title='Edit Donation')
-
-# PUT/PATCH - UPDATE a donation
-@app.route('/donations/<donation_id>', methods=['POST'])
-def donation_update(donation_id):
-  # The newly updated form data
-  updated_donation = {
-    'charity_name': request.form.get('charity_name'),
-    'donation_amount': request.form.get('donation_amount'),
-    'date_donated': request.form.get('date_donated'),
-  }
-  # Set that former donation from db to this updated one
-  donations.update_one(
-    {'_id': ObjectId(donation_id)},
-    {'$set': updated_donation}
-  )
-  return redirect(url_for('donations_index'))
-
-# DELETE - a donation 
-# USING AN <a> TAG WITHOUT POST METHOD WORKS
-@app.route('/donations/<donation_id>/delete')
-def donations_delete(donation_id):
-  print(donation_id)
-  donations.delete_one({'_id': ObjectId(donation_id)})
-  return redirect(url_for('donations_index'))
 
 
 
+
+
+
+""" USER PROFILE ROUTE """
 
 # GET Profile page
 @app.route('/profile')
@@ -114,6 +140,12 @@ def donor_profile():
 
 
 
+
+
+
+
+""" CHARITIES ROUTES """
+
 # GET ALL Charities /charities
 @app.route('/charities')
 def charities_all():
@@ -125,7 +157,6 @@ def charities_all():
 @app.route('/charities/<charity_name>')
 def charity_profile(charity_name):
   charity = charities.find_one({'name': charity_name})
-
   return render_template('charity.html', charity=charity, donations=donations.find({'charity_name': charity_name}))
 
 # # POST - /charities create a new charity when a new one is entered in the donation form
@@ -155,10 +186,18 @@ def charities_update(charity_name):
   return redirect(url_for('charity_profile', charity_name=updated_charity['name']))
 
 
+# DELETE a charity
+@app.route('/charities/<charity_name>/delete')
+def charity_delete(charity_name):
+  print(charity_name)
+  charities.delete_one({'name': charity_name})
+  return redirect(url_for('charities_all'))
 
 
 
 
+
+""" LOGIN ROUTES """
 
 # GET login page
 @app.route('/login')
